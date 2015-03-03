6425c1f630216ef7b5ed66c383e314fb4b26c863
Zachary Schneider <zschneider@berkeley.edu>
2015-03-02 23:26:19 -0800

    finally fixed

--- recycle/js/map.js
+++ recycle/js/map.js
@@ -79,43 +79,44 @@ function initialize() {
 	    navigator.geolocation.getCurrentPosition(function(position) {
 	    var pos = new google.maps.LatLng(position.coords.latitude,
 	                                     position.coords.longitude);
-		}
+		});
 	}
+
 	var map = new google.maps.Map(document.getElementById('map-canvas'), {
     	center: pos,
     	zoom: 10
   	});
 
-  var request = {
-    location: pos,
-    radius: 25000,
-    keyword: 'ewaste electronics recycling'
-  };
+    var request = {
+    	location: pos,
+    	radius: 25000,
+    	keyword: 'ewaste electronics recycling'
+    };
 
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+  	infowindow = new google.maps.InfoWindow();
+  	var service = new google.maps.places.PlacesService(map);
+  	service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
-    }
-  }
+  	if (status == google.maps.places.PlacesServiceStatus.OK) {
+    	for (var i = 0; i < results.length; i++) {
+      		createMarker(results[i]);
+    	}
+  	}
 }
 
 function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
+  	var placeLoc = place.geometry.location;
+  	var marker = new google.maps.Marker({
+    	map: map,
+    	position: place.geometry.location
+  	});
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
+  	google.maps.event.addListener(marker, 'click', function() {
+    	infowindow.setContent(place.name);
+    	infowindow.open(map, this);
+  	});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
