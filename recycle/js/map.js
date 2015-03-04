e53b6c0a65540cdde6aca6fe23bd7a82afd552d1
Zachary Schneider <zschneider@berkeley.edu>
2015-03-03 20:11:51 -0800

    Two diff maps.

--- recycle/js/map.js
+++ recycle/js/map.js
@@ -1,73 +1,3 @@
-// var map;
-
-// function initialize() {
-//   var mapOptions = {
-//     zoom: 5
-//   };
-//   map = new google.maps.Map(document.getElementById('map-canvas'),
-//       mapOptions);
-
-//   // Try HTML5 geolocation
-//   if(navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       var pos = new google.maps.LatLng(position.coords.latitude,
-//                                        position.coords.longitude);
-
-//       var infowindow = new google.maps.InfoWindow({
-//         map: map,
-//         position: pos,
-//         content: 'Location found using HTML5.'
-//       });
-
-//       map.setCenter(pos);
-
-//       var request = {
-//     	location: pos,
-//    	 	radius: '25000',
-//     	keyword: 'electronics recycling ewaste'
-//   	  };
-
-//   	  service = new google.maps.places.PlacesService(map);
-//   	  service.nearbySearch(request, callback);
-
-//     }, function() {
-//       handleNoGeolocation(true);
-//     });
-//   } else {
-//     // Browser doesn't support Geolocation
-//     handleNoGeolocation(false);
-//   }
-// }
-
-
-// function handleNoGeolocation(errorFlag) {
-//   if (errorFlag) {
-//     var content = 'Error: The Geolocation service failed.';
-//   } else {
-//     var content = 'Error: Your browser doesn\'t support geolocation.';
-//   }
-
-//   var options = {
-//     map: map,
-//     position: new google.maps.LatLng(60, 105),
-//     content: content
-//   };
-
-//   var infowindow = new google.maps.InfoWindow(options);
-//   map.setCenter(options.position);
-// }
-
-// function callback(results, status) {
-//   if (status == google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       var place = results[i];
-//       createMarker(results[i]);
-//     }
-//   }
-// }
-
-// google.maps.event.addDomListener(window, 'load', initialize);
-
 var map;
 var infowindow;
 var pos;
@@ -84,6 +14,32 @@ function initialize() {
     		location: pos,
     		radius: 25000,
     		keyword: 'ewaste electronics recycling'
+    	};
+
+    	map = new google.maps.Map(document.getElementById('map-canvas'), {
+    		center: pos,
+    		zoom: 10
+  		});
+    	infowindow = new google.maps.InfoWindow();
+  		
+  		var service = new google.maps.places.PlacesService(map);
+  		service.nearbySearch(request, callback);
+		});
+	}
+}
+
+function newMap() {
+
+  //var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+
+	if(navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(function(position) {
+	    pos = new google.maps.LatLng(position.coords.latitude,
+	                                     position.coords.longitude);
+		request = {
+    		location: pos,
+    		radius: 25000,
+    		keyword: 'recycle recycling'
     	};
 
     	map = new google.maps.Map(document.getElementById('map-canvas'), {
@@ -135,3 +91,5 @@ function createMarker(place) {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(document.getElementById('reg_rec'), 'click', newMap);
+google.maps.event.addDomListener(document.getElementById('ewaste'), 'click', initialize);
